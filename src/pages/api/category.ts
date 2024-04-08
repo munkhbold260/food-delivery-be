e3e6f0bd253f8e16c9dff0817144e67a25386dcb
bold_max <munkhbold260@gmail.com>
2024-04-08 23:16:50 +0800

    cat del

--- src/pages/api/category.ts
+++ src/pages/api/category.ts
@@ -1,6 +1,11 @@
 import { corsAllow } from "@/helper/cors";
 import connect from "@/helper/db";
-import { createCategory, getCategories } from "@/services/category";
+import {
+  createCategory,
+  deleteCategory,
+  getCategories,
+  updateCategory,
+} from "@/services/category";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
@@ -19,10 +24,28 @@ export default async function handler(
       } catch (e: any) {
         return res.status(400).json({ message: e.message });
       }
+
     case "GET":
       try {
         const categories = await getCategories();
         return res.status(200).json({ categories: categories });
+      } catch (e: any) {
+        return res.status(400).json({ message: e.message });
+      }
+
+    case "DELETE":
+      try {
+        const category = await deleteCategory(body.name);
+        return res.status(200).json({ category: category });
+      } catch (e: any) {
+        return res.status(400).json({ message: e.message });
+      }
+
+    case "PUT":
+      try {
+        const category = await updateCategory(body.name);
+        console.log(body.name);
+        return res.status(200).json({ category: category });
       } catch (e: any) {
         return res.status(400).json({ message: e.message });
       }
--- src/services/category.ts
+++ src/services/category.ts
@@ -24,17 +24,19 @@ export const getCategoryById = async (id: string) => {
   }
 };
 
-export const updateCategory = async (id: string) => {
+export const deleteCategory = async (name: string) => {
   try {
-    await CategoryModel.deleteOne({ _id: id });
+    await CategoryModel.deleteOne({ name: name });
   } catch (e: any) {
     throw new Error(e.message);
   }
 };
 
-export const deleteCategory = async (id: string) => {
+export const updateCategory = async (name: string, newName: string) => {
   try {
-    await CategoryModel.deleteOne({ _id: id });
+    await CategoryModel.deleteOne({ name: name });
+    const createCategory = await CategoryModel.create({ newName });
+    return createCategory;
   } catch (e: any) {
     throw new Error(e.message);
   }
